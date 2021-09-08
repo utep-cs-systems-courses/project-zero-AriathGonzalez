e058dc15005cea6db9c3d172bceddb552286e38a
Ariath <asgonzalez11@miners.utep.edu>
2021-09-08 11:47:08 -0600

    Added print arrow method

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,10 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+void print_arrow (int leftCol, int size)
+{
+ 
+  print_triangle(leftCol, size);
+  
+  print_square(leftCol * 2 -1, size);
+}
